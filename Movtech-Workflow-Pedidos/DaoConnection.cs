ce605b778989194e5d89ee1367fda145c125b09d
Gustavo Kauan Storch <112282756+GustavoStorch@users.noreply.github.com>
2023-06-06 08:52:41 -0300

    Add files via upload

---
+++ Movtech-Workflow-Pedidos/DaoConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movtech_Workflow_Pedidos
+{
+    public class DaoConnection
+    {
+        public static SqlConnection GetConexao()
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=localhost\sqlexpress;Initial Catalog=movtech;Integrated Security=True;MultipleActiveResultSets=true;");
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ Movtech-Workflow-Pedidos/FormWorkflowPedidos.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Movtech_Workflow_Pedidos
+{
+    partial class FormWorkflowPedidos
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWorkflowPedidos));
+            this.lblDateDe = new System.Windows.Forms.Label();
+            this.lblDataAte = new System.Windows.Forms.Label();
+            this.lblNomeCliente = new System.Windows.Forms.Label();
+            this.lblPedido = new System.Windows.Forms.Label();
+            this.lblDataAtual = new System.Windows.Forms.Label();
+            this.dtpDataDe = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataAte = new System.Windows.Forms.DateTimePicker();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
+            this.txtPedido = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnBuscarPedido = new System.Windows.Forms.Button();
+            this.btnBuscarCliente = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnBaixarEtapa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDateDe
+            // 
+            this.lblDateDe.AutoSize = true;
+            this.lblDateDe.Location = new System.Drawing.Point(12, 17);
+            this.lblDateDe.Name = "lblDateDe";
+            this.lblDateDe.Size = new System.Drawing.Size(70, 20);
+            this.lblDateDe.TabIndex = 0;
+            this.lblDateDe.Text = "Data de:";
+            // 
+            // lblDataAte
+            // 
+            this.lblDataAte.AutoSize = true;
+            this.lblDataAte.Location = new System.Drawing.Point(233, 17);
+            this.lblDataAte.Name = "lblDataAte";
+            this.lblDataAte.Size = new System.Drawing.Size(38, 20);
+            this.lblDataAte.TabIndex = 1;
+            this.lblDataAte.Text = "Até:";
+            // 
+            // lblNomeCliente
+            // 
+            this.lblNomeCliente.AutoSize = true;
+            this.lblNomeCliente.Location = new System.Drawing.Point(12, 57);
+            this.lblNomeCliente.Name = "lblNomeCliente";
+            this.lblNomeCliente.Size = new System.Drawing.Size(62, 20);
+            this.lblNomeCliente.TabIndex = 2;
+            this.lblNomeCliente.Text = "Cliente:";
+            // 
+            // lblPedido
+            // 
+            this.lblPedido.AutoSize = true;
+            this.lblPedido.Location = new System.Drawing.Point(12, 99);
+            this.lblPedido.Name = "lblPedido";
+            this.lblPedido.Size = new System.Drawing.Size(62, 20);
+            this.lblPedido.TabIndex = 3;
+            this.lblPedido.Text = "Pedido:";
+            // 
+            // lblDataAtual
+            // 
+            this.lblDataAtual.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDataAtual.AutoSize = true;
+            this.lblDataAtual.Location = new System.Drawing.Point(824, 9);
+            this.lblDataAtual.Name = "lblDataAtual";
+            this.lblDataAtual.Size = new System.Drawing.Size(132, 20);
+            this.lblDataAtual.TabIndex = 4;
+            this.lblDataAtual.Text = "Data: 01/01/2001";
+            // 
+            // dtpDataDe
+            // 
+            this.dtpDataDe.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataDe.Location = new System.Drawing.Point(88, 12);
+            this.dtpDataDe.Name = "dtpDataDe";
+            this.dtpDataDe.Size = new System.Drawing.Size(139, 26);
+            this.dtpDataDe.TabIndex = 5;
+            // 
+            // dtpDataAte
+            // 
+            this.dtpDataAte.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataAte.Location = new System.Drawing.Point(277, 12);
+            this.dtpDataAte.Name = "dtpDataAte";
+            this.dtpDataAte.Size = new System.Drawing.Size(139, 26);
+            this.dtpDataAte.TabIndex = 6;
+            // 
+            // txtNomeCliente
+            // 
+            this.txtNomeCliente.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNomeCliente.Location = new System.Drawing.Point(88, 54);
+            this.txtNomeCliente.MaxLength = 100;
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(535, 26);
+            this.txtNomeCliente.TabIndex = 7;
+            // 
+            // txtPedido
+            // 
+            this.txtPedido.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPedido.Location = new System.Drawing.Point(88, 96);
+            this.txtPedido.MaxLength = 100;
+            this.txtPedido.Name = "txtPedido";
+            this.txtPedido.Size = new System.Drawing.Size(535, 26);
+            this.txtPedido.TabIndex = 8;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.Location = new System.Drawing.Point(716, 131);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(116, 34);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConsultar.Location = new System.Drawing.Point(840, 131);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(116, 34);
+            this.btnConsultar.TabIndex = 15;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarPedido
+            // 
+            this.btnBuscarPedido.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscarPedido.FlatAppearance.BorderSize = 0;
+            this.btnBuscarPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscarPedido.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBuscarPedido.Location = new System.Drawing.Point(629, 96);
+            this.btnBuscarPedido.Name = "btnBuscarPedido";
+            this.btnBuscarPedido.Size = new System.Drawing.Size(58, 26);
+            this.btnBuscarPedido.TabIndex = 17;
+            this.btnBuscarPedido.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarCliente
+            // 
+            this.btnBuscarCliente.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscarCliente.FlatAppearance.BorderSize = 0;
+            this.btnBuscarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscarCliente.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBuscarCliente.Location = new System.Drawing.Point(629, 54);
+            this.btnBuscarCliente.Name = "btnBuscarCliente";
+            this.btnBuscarCliente.Size = new System.Drawing.Size(58, 26);
+            this.btnBuscarCliente.TabIndex = 16;
+            this.btnBuscarCliente.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(940, 396);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // btnBaixarEtapa
+            // 
+            this.btnBaixarEtapa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBaixarEtapa.Location = new System.Drawing.Point(839, 577);
+            this.btnBaixarEtapa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBaixarEtapa.Name = "btnBaixarEtapa";
+            this.btnBaixarEtapa.Size = new System.Drawing.Size(116, 34);
+            this.btnBaixarEtapa.TabIndex = 19;
+            this.btnBaixarEtapa.Text = "Baixar Etapa";
+            this.btnBaixarEtapa.UseVisualStyleBackColor = true;
+            // 
+            // FormWorkflowPedidos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 618);
+            this.Controls.Add(this.btnBaixarEtapa);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBuscarPedido);
+            this.Controls.Add(this.btnBuscarCliente);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtPedido);
+            this.Controls.Add(this.txtNomeCliente);
+            this.Controls.Add(this.dtpDataAte);
+            this.Controls.Add(this.dtpDataDe);
+            this.Controls.Add(this.lblDataAtual);
+            this.Controls.Add(this.lblPedido);
+            this.Controls.Add(this.lblNomeCliente);
+            this.Controls.Add(this.lblDataAte);
+            this.Controls.Add(this.lblDateDe);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormWorkflowPedidos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WorkFlow - Pedidos";
+            this.Load += new System.EventHandler(this.FormWorkflowPedidos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDateDe;
+        private System.Windows.Forms.Label lblDataAte;
+        private System.Windows.Forms.Label lblNomeCliente;
+        private System.Windows.Forms.Label lblPedido;
+        private System.Windows.Forms.Label lblDataAtual;
+        private System.Windows.Forms.DateTimePicker dtpDataDe;
+        private System.Windows.Forms.DateTimePicker dtpDataAte;
+        private System.Windows.Forms.TextBox txtNomeCliente;
+        private System.Windows.Forms.TextBox txtPedido;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnBuscarPedido;
+        private System.Windows.Forms.Button btnBuscarCliente;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnBaixarEtapa;
+    }
+}
+
